ee61c987c98b00903dfd5adbce80303c8e2e0663
Wellerson Jesus <wellersonjesus@hotmail.com>
2025-12-17 09:16:22 -0300

    refactor(swagger): centraliza documentação na pasta swagger e remove arquivos duplicados ...

--- backend/public/swagger/swagger-initializer.js
+++ backend/public/swagger/swagger-initializer.js
@@ -1,16 +1,11 @@
-window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
-
+window.onload = function () {
   window.ui = SwaggerUIBundle({
-    url: "/swagger.yaml",
-    dom_id: '#swagger-ui',
+    url: "swagger.yaml",
+    dom_id: "#swagger-ui",
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
-    ],
-    plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
     ],
     layout: "StandaloneLayout"
   });
